8f6681e478740c69459a762611ba48adac1e3457
Yukang Chen <moorekang@gmail.com>
2019-06-06 14:17:28 +0800

    first version

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,13 @@
 extern crate hyper;
 extern crate serde;
 
-use std::io::{self, Write};
 use hyper::Client;
+use std::env;
+use url::Url;
 use hyper::rt::{self, Future, Stream};
 
 extern crate serde_json;
-use serde_json::{Result, Value};
-
-
+use serde_json::{Value};
 
 // Define a type so we can return multiple types of errors
 enum FetchError {
@@ -28,36 +27,54 @@ impl From<serde_json::Error> for FetchError {
     }
 }
 
-
 fn fetch_url(url: hyper::Uri) -> impl Future<Item=Value, Error=FetchError> {
-    let client = Client::new();
-
-
-    client
-    // Fetch the url...
+    Client::new()
+        // Fetch the url...
         .get(url)
-    // And then, if we get a response back...
+         // And then, if we get a response back...
         .and_then(|res| {
             // asynchronously concatenate chunks of the body
             res.into_body().concat2()
         })
         .from_err::<FetchError>()
-    // use the body after concatenation
+        // use the body after concatenation
         .and_then(|body| {
             // try to parse as json with serde_json
             let v: Value = serde_json::from_slice(&body)?;
-
             Ok(v)
         })
         .from_err()
 }
 
 
-fn main() {
-    let uri = "http://fanyi.youdao.com/openapi.do?keyfrom=wufeifei&key=716426270&type=data&doctype=json&version=1.1&q=hello".parse().unwrap();
-    rt::run(fetch_url(uri).map(|r| {
-        println!("res: {}", r)
-    }).map_err(|err| {
+fn translate(input: String)  {
+    let uri = Url::parse_with_params("http://fanyi.youdao.com/openapi.do",
+                                     &[
+                                         ("keyfrom", "wufeifei"),
+                                         ("key", "716426270"),
+                                         ("type", "data"),
+                                         ("doctype", "json"),
+                                         ("version", "1.1"),
+                                         ("q", &input)]).unwrap().into_string();
+    rt::run(fetch_url(uri.parse().unwrap()).map(|v| {
+        println!("result: {}", v["translation"]);
+        match v["web"].as_array() {
+            None => (),
+            Some(others) => {
+                for o in others.into_iter() {
+                    println!("{k} => {v} ", k = o["key"].as_string(), v = o["value"]);
+                }
+            }
+
+        }
+    }).map_err(|_err| {
         eprintln!("err")
     }))
+}
+
+fn main() {
+    let mut args: Vec<String> = env::args().collect();
+    let _ = args.remove(0);
+    let input = args.join(" ");
+    translate(input)
 }
